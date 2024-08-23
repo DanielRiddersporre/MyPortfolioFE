61d2092662f0cfa40c0fcb4905f026d411ce504f
daniel.riddersporre@icloud.com <daniel.riddersporre@icloud.com>
2024-08-23 21:39:54 +0200

    Did not add a hero but fixed my Body and my Header. This is better, I think

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,12 @@
 import "../styles/Body.css"
+import imgUrl from "../assets/hero_dr.jpg"
 
 export default function Body() {
     return(
         <>
-            
+            <div className="wrapper px-8 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dicta aliquid, maiores illum ratione nesciunt deleniti. Error saepe sed sit eligendi exercitationem totam, voluptatem ducimus cumque doloribus maiores hic porro perferendis facilis mollitia numquam cupiditate magnam? Mollitia hic repudiandae corrupti ea culpa explicabo deserunt? Asperiores hic, aut quam aperiam laudantium error, dolorem quos quis labore laboriosam odio similique cupiditate dolores, minus sint tempora dicta enim nobis consequatur dignissimos id exercitationem. Modi vero deserunt ab sed rerum delectus eum ratione voluptatibus, similique minus laboriosam ut exercitationem nihil, facere repudiandae tempora voluptate voluptatum doloremque accusamus aut asperiores quae assumenda quaerat dignissimos! Libero?
+            </div>
         </>
     )
 }
