c8cde9c901c4c301a274362e77ab95f79c92dd5c
Lina <linariddersporre@gmail.com>
2024-08-27 10:10:39 +0200

    router fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './shared/LandingPage'
 import LinaPage from './lina/LinaPage'
 import DanielPage from './daniel/DanielPage'
@@ -8,15 +9,17 @@ function App() {
   return (
     <>
       {/* Define routes for the application. For the LandingPage but also for the individual pages */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/lina" element={<LinaPage />} />
-            {/* Routes specific for Linas page goes here */}
-          <Route path="/daniel" element={<DanielPage />} />
-            {/* Routes specific for Daniels page goes here */}
-        </Routes>
-      </Router>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/lina" element={<LinaPage />} />
+              {/* Routes specific for Linas page goes here */}
+            <Route path="/daniel" element={<DanielPage />} />
+              {/* Routes specific for Daniels page goes here */}
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
     </>
   )
 }
